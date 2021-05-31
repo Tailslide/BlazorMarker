65ed2f3f2bb98c0d9c13c5bf5bf882997f21ea80
Tailslide <5757188+Tailslide@users.noreply.github.com>
2021-05-31 11:24:56 -0600

    Add markerjs2 npm reference, our javascript wrapper, configure webpack

---
+++ BlazorMarker/JsLib/src/blazor_markerjs2.js
@@ -0,0 +1,20 @@
+﻿import * as markerjs2 from 'markerjs2';
+
+var markerArea=undefined;
+var ElementId = undefined;
+
+export function initializeArea(elementId) {
+    ElementId = elementId;
+    markerArea = new markerjs2.MarkerArea(document.getElementById(ElementId));
+
+    // register an event listener for when user clicks OK/save in the marker.js UI
+    markerArea.addRenderEventListener(dataUrl => {
+        // we are setting the markup result to replace our original image on the page
+        // but you can set a different image or upload it to your server
+        document.getElementById(ElementId).src = dataUrl;
+    });
+}
+
+export function show() {
+    if (markerArea !== undefined) markerArea.show();
+}
---
+++ BlazorMarker/JsLib/src/index.js
@@ -0,0 +1,11 @@
+﻿import { initializeArea, show } from './blazor_markerjs2';
+
+export function InitializeArea(elementId) {
+    return initializeArea(elementId);
+}
+
+
+export function Show() {
+    return show();
+}
+
